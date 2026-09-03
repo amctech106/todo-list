1d3c2365e7192af008977fd6380c1c93c2da8683
amctech106 <121874402+amctech106@users.noreply.github.com>
2026-09-03 22:39:38 +0500

    changed

---
+++ script.js
@@ -0,0 +1,32 @@
+let inp = document.querySelector("#inp");
+let btn = document.querySelector("#btn");
+let result = document.querySelector("#result");
+let tasks = []
+
+btn.addEventListener("click", function(){
+let task =inp.value.trim();
+
+if (task===""){
+    result.textContent="برائے مہربانی کچھ لکھیں تو صحیح"
+    result.classList.add("urdu")
+    return
+}
+
+tasks.push(task)
+inp.value=""
+
+displayTasks()
+});
+
+function displayTasks(){
+result.textContent="";
+
+tasks.forEach(function(item , index){
+result.innerHTML+=`${index +1} . ${item} <button onclick = "removeTask(${index})">❌</button><br>`
+})
+}
+
+function removeTask(index){
+    tasks.splice(index,1)
+    displayTasks();
+};
